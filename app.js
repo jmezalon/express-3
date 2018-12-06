927b92e3ef5587a2b113fccf3ae6ff7e0d40a2b1
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2018-12-06 17:57:33 -0500

    i completed this homework

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
 const express = require('express');
 const app = express();
 
-app.get('/math/add/', (req, res) => {
+
+app.get('/math/:operator', (req, res) => {
   let obj = {};
+  let operator = req.params.operator
   obj.input = req.query
-  obj.sumString = `${req.query['a']} + ${req.query['b']}`
-  obj.sum = parseInt(req.query['a']) + parseInt(req.query['b'])
-  // console.log(req.query['a'])
+  if (operator === 'add') {
+
+    obj.productString = Object.values(req.query).join(" + ")
+    obj.product = Object.values(req.query).reduce((acc, curr) => {
+      return acc + curr
+    })
+
+  } else if (operator === 'subtract') {
+
+    obj.productString = Object.values(req.query).join(" - ")
+    obj.product = Object.values(req.query).reduce((acc, curr) => {
+      return acc - curr
+    })
+
+  } else if (operator === 'multiply') {
+
+    obj.productString = Object.values(req.query).join(" * ")
+    obj.product = Object.values(req.query).reduce((acc, curr) => {
+      return acc * curr
+    })
+
+  } else if (operator === 'divide') {
+
+
+    obj.productString = Object.values(req.query).join(" / ")
+    obj.product = Object.values(req.query).reduce((acc, curr) => {
+      return acc / curr
+    })
+
+  }
+
+
   res.json(obj)
 
-})
-
-app.get('/math/subtract/', (req, res) => {
-  let obj = {};
-  obj.input = req.query
-  obj.subString = `${req.query.a} - ${req.query.b}`
-  obj.sum = parseInt(req.query.a) - parseInt(req.query.b)
-  res.json(obj)
-})
-
-app.get('/math/multiply/', (req, res) => {
-  let obj = {};
-  obj.input = req.query;
-
-  obj.productString = Object.values(req.query).join(" * ")
-
-  // console.log(Object.values(req.query));
-  obj.product = Object.values(req.query).reduce((acc, curr) => {
-    return acc * curr
-  })
-
-  // check.forEach(el => {
-  //   obj.productString *= `${el}`
-  // })
-
-  // obj.input = req.query;
-  // console.log(req.query)
-  // obj.productString = `${req.query.a} * ${req.query.b}`
-  // obj.product = parseInt(req.query.a) * parseInt(req.query.b)
-  res.json(obj)
-})
-
-app.get('/math/div/', (req, res) => {
-  let obj = {};
-  obj.input = req.query;
-  obj.divideString = `${req.query['a']} / ${req.query['b']}`
-  obj.divide = parseInt(req.query['a'] / parseInt(req.query['b']))
-  res.json(obj)
 })
 
 app.listen(2000, () => {
